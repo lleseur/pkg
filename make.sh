86a0d8ba857faa9f5b999021e90285ad55a49b80
Louis Leseur <louis.leseur@gmail.com>
2020-04-10 02:28:07 +0200

    Renamed make to build, added elevated functionnality
    Signed-off-by: Louis Leseur <louis.leseur@gmail.com>

--- make.sh
+++ make.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 # This script is the standard script used for build, install, removal of programs
 #
-# It uses the 3 functions "make" for building, "install" for installing, "remove"
+# It uses the 3 functions "build" for building, "install" for installing, "remove"
 # to uninstall a program
 # The script will source the ./makerc file from the current package, the file is located
 # in "category/package/makerc" and will override the default functions
 # The default functions are still available as function_default (e.g. install_default)
 # The function "die" is available to stop the process at any point sending an error message
 # The function "elevate" is available to run a command as root (using sudo or su)
+#
+# The functions install_default and remove_default will be run as root if the variables
+# ELEVATED_INSTALL or ELEVATED_REMOVE are not empty
 
 die()
 {
@@ -20,11 +23,11 @@ elevate()
 {
 	[ "$(id -u)" = "0" ] && "$@" && return 0
 	command -v sudo 1>/dev/null 2>/dev/null && echo "Requesting elevated privileges using sudo" && sudo "$@" && return 0
-	su -c "$*" root && return 0
+	echo "Requesting elevated privileges using su" && su -c "$*" root && return 0
 	return 1
 }
 
-make_default()
+build_default()
 {
 	# Load environment variable from env.conf
 	if [ -f "../../env.conf" ]; then
@@ -39,7 +42,7 @@ make_default()
 		./configure || die "Configure failed"
 	fi
 	if [ -f Makefile ] || [ -f makefile ] || [ -f GNUmakefile ]; then
-		eval make "${MAKEOPTS}" || die "Make failed"
+		eval make MAKEOPTS="${MAKEOPTS}" || die "Make failed"
 	fi
 
 	return 0
@@ -49,7 +52,11 @@ install_default()
 {
 	# Try default install process
 	if [ -f Makefile ] || [ -f makefile ] || [ -f GNUmakefile ]; then
-		make DESTDIR="${DESTDIR}" install || die "Make install failed"
+		if [ -n "${ELEVATED_INSTALL}" ]; then
+			elevate make DESTDIR="${DESTDIR}" install || die "Make install failed"
+		else
+			make DESTDIR="${DESTDIR}" install || die "Make install failed"
+		fi
 	fi
 
 	return 0
@@ -59,23 +66,32 @@ remove_default()
 {
 	# Try defaut install process
 	if [ -f Makefile ] || [ -f makefile ] || [ -f GNUmakefile ]; then
-		make DESTDIR="${DESTDIR}" uninstall
+		if [ -n "${ELEVATED_REMOVE}" ]; then
+			elevate make DESTDIR="${DESTDIR}" uninstall || die "Make uninstall failed"
+		else
+			make DESTDIR="${DESTDIR}" uninstall || die "Make uninstall failed"
+		fi
 	fi
 
 	return 0
 }
 
-make() { make_default; }
+
+build() { build_default; }
 install() { install_default; }
 remove() { remove_default; }
 
-if [ -x "./makerc" ]; then
+# Read makerc
+if [ -f "./makerc" ]; then
 	. "./makerc"
 fi
+
+# Parse arguments
+[ $# -eq 0 ] && build || die "Build failed"
 for arg in "$@"; do
 	case "$arg" in
-	""|all|make)
-		make || die "Build failed"
+	""|all|make|build)
+		build || die "Build failed"
 		;;
 	remove)
 		remove || die "Remove failed"
